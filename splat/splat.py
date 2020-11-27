Corrects input-output size computation

splat/splat.py
@@ -4,6 +4,7 @@ from splunklib.client import connect
 
 import click
 import json
+import numpy as np
 import os
 import pandas as pd
 import re
@@ -35,43 +36,55 @@ def get_file_size(file_name: str) -> int:
     return stat(file_name).st_size
 
 
-def convert_kv_to_string(kv_pairs: list) -> str:
+def convert_kv_to_string(kv_pairs: list) -> list:
     kv_str: list = []
     for kv_pair in kv_pairs:
         temp: list = []
         for k, v in kv_pair.items():
             if isinstance(v, str):
                 temp.append(f"{k}={v}")
-            elif (isinstance(v, (int, float))) and (not isnan(v)):
+            elif (isinstance(v, (int, np.integer, float, np.float))) and (not isnan(v)):
                 temp.append(f"{k}={v}")
 
         kv_str.append(" ".join(temp))
 
-    return "\n".join(kv_str)
+    return kv_str
 
 
-def get_size_on_disk(kv_pairs: list) -> int:
+def convert_kv_to_logs(kv_pairs: list, verbatim_text: list):
+    str_kv_pairs: list = convert_kv_to_string(kv_pairs)
+    str_verbatim_text: list = [" ".join(x) for x in verbatim_text]
+
+    return [f"{kv_pair} {verb_text}" for kv_pair, verb_text in zip(str_kv_pairs, str_verbatim_text)]
+
+
+def get_size_on_disk(logs: list) -> int:
     tmp_file_name: str = "tmp.txt"
     with open(tmp_file_name, "w") as f:
-        kv_pairs_str: str = convert_kv_to_string(kv_pairs)
-        f.write(kv_pairs_str)
+        logs_text: str = "\n".join(logs)
+        f.write(logs_text)
     file_size: int = get_file_size(tmp_file_name)
     os.remove(tmp_file_name)
 
     return file_size
 
 
-def get_key_value_pairs(text: list) -> list:
-    key_value_pairs = []
+def get_key_value_pairs(text: list) -> (list, list):
+    key_value_pairs: list = []
+    verbatim_content: list = []
     for line in text:
         kv_pairs: dict = {}
+        non_kv_content: list = []
         for pair in shlex.split(line):
             if "=" in pair:
                 kv_pair: list = pair.split("=")
                 kv_pairs[kv_pair[0]] = kv_pair[1]
+            else:
+                non_kv_content.append(pair)
         key_value_pairs.append(kv_pairs)
+        verbatim_content.append(non_kv_content)
 
-    return key_value_pairs
+    return key_value_pairs, verbatim_content
 
 
 def evaluate_ip_addr_freq(values: pd.Series, freq_thresh: float) -> bool:
@@ -127,34 +140,22 @@ def convert_values_to_tokens(kv_df: pd.DataFrame, cols: list):
     return kv_df
 
 
-def estimate_log_compression(log_kv_pairs: list, freq_threshold: float) -> dict:
+def dedup_kv_pairs(log_kv_pairs: list, freq_threshold: float) -> list:
     """
-    Compute pre- and post-compression file size of an input list of key-value of pairs.
+    Convert frequently occuring values into integer tokens.
 
     :param log_kv_pairs: List of key-value pairs.
     :param freq_threshold: Minimum proportion required from the value of a key for the key to be converted to a
     template.
-    :return: Dictionary containing size (in bytes) of input and compressed key-value pairs when written to a `.txt`
-    file, percentage of compression achieved, and keys to use as templates.
+    :return: List of key-value pairs with frequent values deduped.
     """
-    input_size: int = get_size_on_disk(log_kv_pairs)
     log_kv_df: pd.DataFrame = pd.DataFrame(log_kv_pairs)
 
     keys_to_templatise: list = get_signif_keys_using_freq(log_kv_df, freq_threshold)
     log_kv_df: pd.DataFrame = convert_values_to_tokens(log_kv_df, keys_to_templatise)
     compressed_log_kv_pairs: list = log_kv_df.to_dict("records")
 
-    output_size: int = get_size_on_disk(compressed_log_kv_pairs)
-    percent_reduction: float = round((input_size - output_size) * 100 / input_size, 2)
-
-    compression_info: dict = {
-        "input_size": input_size,
-        "output_size": output_size,
-        "percent_reduction": percent_reduction,
-        "keys_to_dedup": keys_to_templatise
-    }
-
-    return compression_info
+    return compressed_log_kv_pairs
 
 
 def get_splunk_connection(splunk_config: dict) -> client.Service:
@@ -170,8 +171,11 @@ def get_splunk_connection(splunk_config: dict) -> client.Service:
     return service
 
 
-def construct_search_args(earliest_time: str, latest_time: str) -> dict:
-    search_args: dict = dict(exec_mode="normal")
+def construct_query_args(earliest_time: str, latest_time: str, query_type: str) -> dict:
+    if query_type == "event_count":
+        search_args: dict = dict(exec_mode="normal")
+    else:
+        search_args: dict = dict(search_mode="normal")
     search_args["earliest_time"] = earliest_time
     search_args["latest_time"] = latest_time
 
@@ -185,7 +189,7 @@ def check_for_sufficient_data(event_count: int) -> None:
 
 
 def check_processing_time(event_count: int, verbose: bool, warning_thresh: int = 180) -> None:
-    approx_processing_time = (event_count / 100000) * 10
+    approx_processing_time = round((event_count / 100000) * 10, 2)
     if verbose:
         print(f"Fetching {event_count} records from Splunk will require approximately "
               f"{approx_processing_time} minutes.")
@@ -210,7 +214,8 @@ def get_splunk_query_results(splunk_connection: client.Service, query: str, sear
         time.sleep(.2)
 
     count = 0
-    rr = results.ResultsReader(job.results(count=0))  # Max number of records in one poll 50k.
+    # rr = results.ResultsReader(job.results(count=0))  # Max number of records in one poll 50k.
+    rr = results.ResultsReader(splunk_connection.jobs.export(query, **search_args))
     for result in rr:
         if isinstance(result, dict):
             query_results.append(result["_raw"])
@@ -244,9 +249,10 @@ def get_event_count(splunk_connection: client.Service, query: str, search_args: 
 def splat(splunk_details: dict, index: str, start_time: str, end_time: str, num_records: int, verbose: bool):
     search_query: str = "search index={0}".format(index)
     splunk_conn: client.Service = get_splunk_connection(splunk_details)
-    search_params: dict = construct_search_args(start_time, end_time)
+    search_params: dict = construct_query_args(start_time, end_time, query_type="search")
+    event_count_params: dict = construct_query_args(start_time, end_time, query_type="event_count")
 
-    total_event_count: int = get_event_count(splunk_conn, search_query, search_params, verbose)
+    total_event_count: int = get_event_count(splunk_conn, search_query, event_count_params, verbose)
     check_for_sufficient_data(total_event_count)
     check_processing_time(total_event_count, verbose)
 
@@ -255,12 +261,20 @@ def splat(splunk_details: dict, index: str, start_time: str, end_time: str, num_
     if verbose:
         print(f"Number of records retrieved from Splunk: {len(splunk_logs)}")
 
-    log_key_value_pairs: list = get_key_value_pairs(splunk_logs)
-    compression_stats: dict = estimate_log_compression(log_key_value_pairs, 0.9)
+    input_size: int = get_size_on_disk(splunk_logs)
 
-    print(f"Input data size: {round(compression_stats['input_size'] / 1024, 2)} KBs")
-    print(f"Output data size: {round(compression_stats['output_size'] / 1024, 2)} KBs")
-    print(f"Estimated compression: {compression_stats['percent_reduction']}%")
+    log_key_value_pairs: list
+    log_verbatim_text: list
+    log_key_value_pairs, log_verbatim_text = get_key_value_pairs(splunk_logs)
+    deduped_key_value_pairs: list = dedup_kv_pairs(log_key_value_pairs, 0.9)
+    deduped_splunk_logs: list = convert_kv_to_logs(deduped_key_value_pairs, log_verbatim_text)
+
+    output_size: int = get_size_on_disk(deduped_splunk_logs)
+    percent_reduction: float = round((input_size - output_size) * 100 / input_size, 2)
+
+    print(f"Input data size: {round(input_size / 1024, 2)} KBs")
+    print(f"Output data size: {round(output_size / 1024, 2)} KBs")
+    print(f"Estimated compression: {percent_reduction}%")
 
 
 @click.command()
@@ -269,9 +283,9 @@ def splat(splunk_details: dict, index: str, start_time: str, end_time: str, num_
                                                                       "README for details.")
 @click.option("-i", "--index", required=True, help="Splunk index to fetch results.")
 @click.option("-st", "--start-time", required=True, help=f"Query start time. It is specified in relation to the current"
-                                                         f"time. So `2d` means 2 days prior to now.")
+                                                         f"time. So `-2d` means 2 days prior to now.")
 @click.option("-et", "--end-time", required=True, help=f"Query end time. It can either be `now` or a time in the past"
-                                                       f"from the present like `2d`.")
+                                                       f"from the present like `-2d`.")
 @click.option("-n", type=int, default=0, help="Number of records to limit the query to.")
 @click.option("-v", "--verbose", is_flag=True)
 def main(splunk_config: str, index: str, start_time: str, end_time: str, n: int, verbose: bool):
@@ -280,7 +294,8 @@ def main(splunk_config: str, index: str, start_time: str, end_time: str, n: int,
     \b
     Examples:
         splat --splunk-config splunk.json --index firewall -n 5000 -v
-        splat --splunk-config splunk.json --index firewall --start-time 2020-11-10T12:00:00.000-00:00 --end-time 2020-11-10T13:00:00.000-00:00
+        splat --splunk-config splunk.json --index firewall --start-time 2020-11-10T12:00:00.000-00:00
+        --end-time 2020-11-10T13:00:00.000-00:00
     """
     with open(splunk_config, "r") as f:
         splunk_server_details: dict = json.load(f)
@@ -289,3 +304,4 @@ def main(splunk_config: str, index: str, start_time: str, end_time: str, n: int,
 
 if __name__ == '__main__':
     main()
+    # main(["-sc", "../splunk.json", "-i", "firewall", "-st", "-30m", "-et", "now"])  # For debugging.